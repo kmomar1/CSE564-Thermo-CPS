9fb0b236d78bdec52b89f9a8d1f85aa336cd0367
Karim Omar <kmomar1@asu.edu>
2025-11-30 18:49:31 -0700

    chore: project init

---
+++ src/main/java/com/example/cse564/Cse564Application.java
@@ -0,0 +1,13 @@
+package com.example.cse564;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cse564Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cse564Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cse564/cse564/Cse564ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cse564.cse564;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cse564ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
