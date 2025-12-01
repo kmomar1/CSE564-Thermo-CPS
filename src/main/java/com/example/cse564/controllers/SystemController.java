7cadb5acd94f0cf465e37ab986ef2fd70366d881
Karim Omar <kmomar1@asu.edu>
2025-11-30 22:02:00 -0700

    feat: Starter code for unified system controller

---
+++ src/main/java/com/example/cse564/controllers/SystemController.java
@@ -0,0 +1,31 @@
+package com.example.cse564.controllers;
+
+import com.example.cse564.models.Grill;
+import com.example.cse564.models.Thermometer;
+import com.example.cse564.models.Phone;
+import com.example.cse564.models.foodprofiles.FoodProfile;
+
+public class SystemController {
+  private Grill grill;
+  private Thermometer thermometer;
+  private Phone phone;
+  
+  public SystemController() {
+    this.grill = Grill.getInstance();
+    this.thermometer = Thermometer.getInstance();
+    this.phone = Phone.getInstance();
+  }
+  
+  public void startCooking(FoodProfile profile) {
+    phone.setSelectedFoodProfile(profile);
+    grill.turnGrillOn();
+  }
+  
+  public void stopCooking() {
+    grill.turnGrillOff();
+  }
+  
+  public void simulateStep() {
+  }
+}
+
